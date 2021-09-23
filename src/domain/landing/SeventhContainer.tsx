3e8bce537a44b15767d48a34bbe485a37a471511
Mustafa Akın İpek <akinipek@Mustafas-MacBook-Pro.local>
2021-09-23 21:02:58 +0200

    changes on iphone an

--- src/domain/landing/SeventhContainer.tsx
+++ src/domain/landing/SeventhContainer.tsx
@@ -10,7 +10,7 @@ interface ISixthContainer {
 
 const SeventhComponent = styled.div<ISixthContainer>`
   background-color: #121420;
-  margin-top: 20%;
+  margin-top: 10%;
   height: 1050px;
   animation: 1s ease-out 0s 1 slideInFromLeft;
 
