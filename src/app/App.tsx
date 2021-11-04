f1c42329824944d9ed43e377f2bfc6a7224490bd
akinipek <aipek@deloitte.nl>
2021-11-04 23:40:20 +0100

    adding router for minting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Landing from "../domain/landing/landing";
 import Web3 from "web3";
 import { Web3ReactProvider } from "@web3-react/core";
+import { Minting } from "../domain/minter/Minting";
+import {Route, Switch } from "react-router-dom";
+    
 
 function getLibrary(provider: any) {
   return new Web3(provider);
@@ -12,7 +15,10 @@ function App() {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <div className="MainApp">
-        <Landing />
+        <Switch>
+          <Route path="/" component={Landing} exact />
+          <Route path="/mint" component={Minting} />
+        </Switch>
       </div>
     </Web3ReactProvider>
   );
---
+++ src/domain/minter/Minting.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components";
+
+const MinterContainer = styled.div`
+    background-color: red;
+    height: 500px;
+    width: 200px;
+`;
+
+export const Minting: React.FC = () => {
+    return (
+        <MinterContainer>
+            <h1>Amk</h1>
+        </MinterContainer>
+    )
+}
+
